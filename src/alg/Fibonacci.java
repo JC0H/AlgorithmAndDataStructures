929943ed73f924aa7b60dec92f658d5910644ce0
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-09-20 21:08:45 +0200

    Fib with tests

--- src/alg/Fibonacci.java
+++ src/alg/Fibonacci.java
@@ -1,31 +1,43 @@
 package alg;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class Fibonacci {
 
     static int fibRec(int n){
-        if (n <= 1)
-            return n;
-        return fibRec(n-1) + fibRec(n-2);
+       if (n <= 1){
+           return n;
+       }
+       return fibRec(n - 1) +fibRec( n - 2);
     }
 
     static int fibIter(int n){
-        int f[] = new int[n+2];
+        int fib[] = new int[n+2];
 
-        f[0] = 0;
-        f[1] = 1;
+        fib[0] = 0;
+        fib[1] = 1;
 
-        for (int i = 2; i <= n; i++)
-        {
-            f[i] = f[i-1] + f[i-2];
+        for (int i = 2; i <= n; i++){
+            fib[i] = fib[i-1] + fib[i-2];
         }
-
-        return f[n];
+        return fib[n];
     }
 
-    public static void main (String args[])
-    {
-        int n = 7;
-        System.out.println(fibRec(n));
-        System.out.println(fibIter(n));
+    @Test
+    public void solutionIter(){
+        int actual = 7;
+        int expected = 13;
+        assertEquals(expected, new Fibonacci().fibIter(actual));
+    }
+
+    @Test
+    public void solutionRec(){
+        int actual = 7;
+        int expected = 13;
+        assertEquals(expected, new Fibonacci().fibRec(actual));
     }
 }
