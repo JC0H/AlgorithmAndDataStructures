402c8f530ff45fd5136f2b2e447b600a3402d011
andri <andriiandrosiukk@gmail.com>
2019-07-11 06:14:20 +0200

    add weight to UF alg

--- src/UnionFind/UFQuickUnion.java
+++ src/UnionFind/UFQuickUnion.java
@@ -3,35 +3,45 @@ package UnionFind;
 public class UFQuickUnion {
     private int[] id; // access to component id (site indexed)
     private int count; // number of components
-
+    private int[] sz; // access to size of components for root
     public UFQuickUnion (int N) { // Initialize component id array.
         count = N;
         id = new int[N];
         for (int i = 0; i < N; i++)
             id[i] = i;
+        sz = new int[N];
+        for (int i = 0; i < N; i++) sz[i] = 1;
     }
 
     public int count() {
         return count;
     }
 
-    public boolean connected(int p, int q) {
-        return find(p) == find(q);
+    private int root(int e){
+        while (e != id[e]){
+            id[e] = id[id[e]]; // path compression - make every next node refer to grandparent node
+            e = id[e];
+        }
+        return e;
     }
 
-    private int find(int p){
-        while (p != id[p]) p = id[p];
-        return p;
+    private boolean connected(int p, int q){
+        return root(p) == root(q);
     }
 
     public void union(int p, int q){
-        int idP = find(p);
-        int idQ = find(q);
-
+        int idP = root(p);
+        int idQ = root(q);
         if (idP == idQ) return;
-
+        //check what tree is bigger
+        if (sz[idP] < sz[idQ]){
+            id[idP] = idQ;
+            sz[idQ] += sz[idP];
+        }else{
+            id[idQ] = idP;
+            sz[idP] += sz[idQ];
+        }
         id[idP] = idQ;
-        count--;
     }
 
     public static void main(String[] str)
@@ -39,19 +49,11 @@ public class UFQuickUnion {
         UFQuickUnion uf = new UFQuickUnion(10);
 
         uf.union(9, 5);
-        System.out.println(uf);
         uf.union(8, 7);
-        System.out.println(uf);
         uf.union(3, 9);
-        System.out.println(uf);
         uf.union(4, 8);
-        System.out.println(uf);
         uf.union(7, 0);
-        System.out.println(uf);
         uf.union(5, 1);
-        System.out.println(uf);
-
-        //System.out.println(uf);
     }
 
 }
