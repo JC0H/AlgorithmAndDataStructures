88c94438520f57e11782d8b012307ccd938a1135
andri <andriiandrosiukk@gmail.com>
2019-07-12 05:58:05 +0200

    sorting example

---
+++ src/Sorting/Example.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class Example {
+
+    public void sort(Comparable[] a){
+
+    }
+
+    public void print(Comparable[] l){
+        for (int i = 0; i < l.length; i++)
+            System.out.print(l[i] + " ");
+    }
+
+    private static boolean less (Comparable a, Comparable b){
+        return a.compareTo(b) < 0;
+    }
+
+    public static void exch(Comparable a[], int i, int j){
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static boolean isSorted( Comparable[] a){
+        for (int i = 0; i < a.length; i++)
+            if(less(a[i],a[i-1])) return false;
+        return true;
+    }
+
+}
