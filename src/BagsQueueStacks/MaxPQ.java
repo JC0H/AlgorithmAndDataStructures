f4bd3651352e1066e89c2d12898cc1eec0de0a17
andri <andriiandrosiukk@gmail.com>
2019-07-19 19:32:29 +0200

    Max PQ class with methods to implement

---
+++ src/BagsQueueStacks/MaxPQ.java
@@ -0,0 +1,28 @@
+package BagsQueueStacks;
+
+public class MaxPQ< Key extends Comparable<Key>> {
+
+    void insert(Key v){
+
+    }
+
+    //return the largest key
+    Key max(){
+        return null;
+    }
+
+    //return and remove the largest key
+    Key delMax() {
+        return null;
+    }
+
+    //is the priority queue empty?
+    boolean isEmpty() {
+        return false;
+    }
+
+    //number of keys in the priority queue
+    int size() {
+        return 0;
+    }
+}
