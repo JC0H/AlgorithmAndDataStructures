1ef3c51ffffaa798c73001af9d5ee966a9e42c3b
andri <andriiandrosiukk@gmail.com>
2019-07-30 07:14:07 +0200

    Black Red trees impl

--- src/BagsQueueStacks/BRT.java
+++ src/BagsQueueStacks/BRT.java
@@ -13,6 +13,12 @@ public class BRT<Key extends Comparable<Key>, Value> {
         int N;
         boolean color;
 
+        public Node(Key key, Value value, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+        }
+
         public Node(Key key, Value value, int n, boolean color) {
             this.key = key;
             this.value = value;
@@ -44,6 +50,45 @@ public class BRT<Key extends Comparable<Key>, Value> {
         x.color = h.color;
         h.color = RED;
         return x;
+    }
+
+    private Value get(Key key){
+        Node x = root;
+        while(x != null){
+            int cmp = key.compareTo(x.key);
+            if      (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else              return x.value;
+        }
+        return null;
+    }
+
+    private void flipColors(Node n){
+        assert !isRed(n);
+        assert  isRed(n.left);
+        assert isRed(n.right);
+        n.color = RED;
+        n.left.color = BLACK;
+        n.right.color = BLACK;
+    }
+
+    /*
+    *Right child red, left child black : rotate left
+    *Left child, left-left child grandchild red : rotate rotate right
+    *Both children red : flipColors
+    */
+    private Node put(Node h, Key key, Value value){
+        if (h ==  null) return new Node(key,value,RED);
+        int cmp =  key.compareTo(h.key);
+        if      (cmp < 0) h.left = put(h.left,key,value);
+        else if (cmp > 0) h.right = put(h.right,key,value);
+        else              h.value = value;
+
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left))  h = rotateRight(h);
+        if (isRed(h.right)  && isRed(h.right))   flipColors(h);
+
+        return h;
     }
 
 }
