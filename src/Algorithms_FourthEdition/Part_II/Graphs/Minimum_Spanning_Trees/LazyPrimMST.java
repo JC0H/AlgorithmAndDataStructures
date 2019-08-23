fc76d0151e452b220dc8459078a898ab7c04f52b
andri <andriiandrosiukk@gmail.com>
2019-08-23 22:05:19 +0200

    Lazy impl

---
+++ src/Algorithms_FourthEdition/Part_II/Graphs/Minimum_Spanning_Trees/LazyPrimMST.java
@@ -0,0 +1,37 @@
+package Algorithms_FourthEdition.Part_II.Graphs.Minimum_Spanning_Trees;
+
+import Algorithms_FourthEdition.Part_I.I_Fundamentals.BagsQueueStacks.Queue;
+import Algorithms_FourthEdition.Part_I.I_Fundamentals.BagsQueueStacks.dataStructures.MinPQ;
+
+public class LazyPrimMST {
+    private boolean [] marked; //MST vertices
+    private Queue <Edge> mst;
+    private MinPQ <Edge> pq;
+
+    public LazyPrimMST(EdgeWeightedGraph G) {
+        pq = new MinPQ<>();
+        mst = new Queue<>();
+        marked = new boolean[G.V()];
+
+        visit(G, 0);
+        while (!pq.isEmpty()){
+            Edge e =  pq.delMin();
+            int v = e.either(), w = e.other(v);
+            if (marked[v] && marked[w]) continue;
+            mst.enqueue(e);
+            if (!marked[v]) visit(G, v);
+            if (!marked[w]) visit(G, w);
+        }
+
+    }
+
+    private void visit(EdgeWeightedGraph G, int v) {
+        marked[v] = true;
+        for (Edge e : G.adj(v)){
+            if (!marked[e.other(v)])
+                pq.insert(e);
+        }
+    }
+
+
+}
